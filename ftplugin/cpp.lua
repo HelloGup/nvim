06cfbe20d2d2a4ea4191ed491d02c773237886f8
Gu Yp <gu.yp@qq.com>
2023-05-21 18:47:05 +0800

    update

--- ftplugin/cpp.lua
+++ ftplugin/cpp.lua
@@ -8,20 +8,20 @@ vim.keymap.set(
   "n",
   "<leader>ma",
   ":split<CR>:te make && ./%:t:r<CR>i",
-  { desc="make",silent = true, noremap = true }
+  { desc = "make", silent = true, noremap = true }
 )
 
 vim.keymap.set(
   "n",
   "<leader>mc",
   ":split<CR>:te make clean<CR>i",
-  {desc="make clean", silent = true, noremap = true }
+  { desc = "make clean", silent = true, noremap = true }
 )
 
 -- 生成同名可执行文件运行
 vim.keymap.set(
   "n",
   "<leader>mr",
-  ":split<CR>:te g++ -std=c++11 -o ./%:t:r % -g && ./%:t:r<CR>i",
-  { desc="g++ run",silent = true, noremap = true }
+  ":split<CR>:te g++ -std=c++11 -o ./%:t:r % -lpthread -g && ./%:t:r<CR>i",
+  { desc = "g++ run", silent = true, noremap = true }
 )
